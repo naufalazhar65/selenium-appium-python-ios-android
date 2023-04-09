7280057f803e21c6ae04c2daccfd4bcaae78d17b
Naufal Azhar <naufalazhar65@gmail.com>
2023-04-10 00:39:10 +0700

    commit

--- test_script.py
+++ test_script.py
@@ -1,58 +1,189 @@
+import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import pytest
-import time
+from selenium.webdriver.chrome.options import Options
+from time import sleep
 
-options = Options()
-options.add_argument("start-maximized")
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--ignore-ssl-errors')
-options.add_argument('--headless')
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-
-def test_setup():
-    global driver
-    driver = webdriver.Chrome("/Users/naufalazhar/Documents/ChromeDriver/chromedriver")
+@pytest.fixture
+def browser():
+    options = Options()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    # options.add_argument('--headless')
+    
+    driver = webdriver.Chrome("/Users/naufalazhar/Documents/ChromeDriver/chromedriver", options=options)
     driver.maximize_window()
     driver.implicitly_wait(15)
-    print("Setup Completed")
-
-def test_get():
-    driver.get("https://myjobstreet.jobstreet.co.id/home/login.php?site=id")
-    assert driver.title == "Mendaftar atau masuk untuk melamar lowongan @ JobStreet.com"
-
-def test_login():
-    username = driver.find_element(by=By.ID, value='login_id')
-    password = driver.find_element(by=By.XPATH, value="//input[@type='password']")
-    submit_button = driver.find_element(by=By.NAME, value='btn_login')
-    username.send_keys('naufalazhar@gmail.com')
-    password.send_keys('********')
-    time.sleep(2)
-    submit_button.click()
-
-def test_findjob():
-    assert 'Selamat Datang Naufal' in driver.find_element(by=By.CSS_SELECTOR, value='h3').text
-    search_field = driver.find_element(by=By.ID, value='searchKeywordsField')
-    location_field = driver.find_element(by=By.ID, value='locationAutoSuggest')
-    search_field.send_keys("Quality Assurance")
-    location_field.send_keys("Jakarta" + Keys.ENTER)
-    time.sleep(2)
-    assert "Quality Assurance" in driver.page_source and "Jakarta" in driver.page_source
-    print("Test Completed")
-
-def test_profil():
-    driver.get("https://myjobstreet.jobstreet.co.id/resume/preview-resume.php")
-    assert driver.title == "MyJobStreet - Lihat Profil Saya"
-    assert 'Azhar' in driver.find_element(by=By.CSS_SELECTOR, value='div').text
-    time.sleep(3)
-
-def test_teardown():
-    driver.close()
+    yield driver
     driver.quit()
+
+# ==================================================================================================
+
+def test_valid_login(browser):
+    browser.get('https://www.saucedemo.com/')
+    assert 'Swag Labs' in browser.title
+
+    username = browser.find_element(by=By.ID, value='user-name')
+    password = browser.find_element(by=By.ID, value='password')
+    login_button = browser.find_element(by=By.ID, value='login-button')
+    username.send_keys("standard_user")
+    password.send_keys("secret_sauce")
+    login_button.click()
+    sleep(2)
+
+    assert "Products" in browser.page_source and "Swag Labs" in browser.page_source
+    assert browser.current_url == 'https://www.saucedemo.com/inventory.html'
+
+# ==================================================================================================
+
+def test_invalid_login(browser):
+    browser.get('https://www.saucedemo.com/')
+    assert 'Swag Labs' in browser.title
+
+    username = browser.find_element(by=By.ID, value='user-name')
+    password = browser.find_element(by=By.ID, value='password')
+    login_button = browser.find_element(by=By.ID, value='login-button')
+    username.send_keys("invalid_user")
+    password.send_keys("invalid_password")
+    login_button.click()
+    sleep(2)
+
+    assert browser.find_element(by=By.CSS_SELECTOR, value='.error-button').is_displayed()
+    assert "Epic sadface: Username and password do not match any user in this service" in browser.page_source
+
+# ==================================================================================================
+
+def test_logout(browser):
+    browser.get('https://www.saucedemo.com/')
+    assert 'Swag Labs' in browser.title
+
+    username = browser.find_element(by=By.ID, value='user-name')
+    password = browser.find_element(by=By.ID, value='password')
+    login_button = browser.find_element(by=By.ID, value='login-button')
+    username.send_keys("standard_user")
+    password.send_keys("secret_sauce")
+    login_button.click()
+    sleep(2)
+    assert "Products" in browser.page_source and "Swag Labs" in browser.page_source
+
+    menu_button = browser.find_element(by=By.ID, value='react-burger-menu-btn')
+    menu_button.click()
+    logout_button = browser.find_element(by=By.ID, value='logout_sidebar_link')
+    logout_button.click()
+    sleep(2)
+    assert "Swag Labs" in browser.title
+    assert "Username" in browser.find_element(by=By.ID, value='user-name').get_attribute("placeholder")
+
+# ==================================================================================================
+
+def test_checkout(browser):
+    browser.get('https://www.saucedemo.com/')
+    assert 'Swag Labs' in browser.title
+
+    username = browser.find_element(by=By.ID, value='user-name')
+    password = browser.find_element(by=By.ID, value='password')
+    login_button = browser.find_element(by=By.ID, value='login-button')
+    username.send_keys("standard_user")
+    password.send_keys("secret_sauce")
+    login_button.click()
+    sleep(2)
+    
+    assert "Products" in browser.page_source and "Swag Labs" in browser.page_source
+
+    # Add item to cart
+    item = browser.find_element(by=By.ID, value='add-to-cart-sauce-labs-bike-light')
+    item.click()
+    assert "Remove" in browser.find_element(by=By.XPATH, value="//button[@name='remove-sauce-labs-bike-light']").text
+    assert "1" in browser.find_element(by=By.CLASS_NAME, value="shopping_cart_badge").text
+    sleep(2)
+
+    # Go to cart
+    cart_button = browser.find_element(by=By.CSS_SELECTOR, value='.shopping_cart_badge')
+    cart_button.click()
+    sleep(2)
+
+    assert "Your Cart" in browser.page_source
+    assert "$9.99" in browser.find_element(by=By.CLASS_NAME, value="inventory_item_price").text
+
+    # Proceed to checkout
+    checkout_button = browser.find_element(by=By.ID, value='checkout')
+    checkout_button.click()
+    sleep(2)
+
+    # Fill in checkout form
+    assert "Checkout: Your Information" in browser.page_source
+    assert browser.find_element(by=By.CLASS_NAME, value="checkout_info").is_displayed()
+
+    firstname = browser.find_element(by=By.ID, value='first-name')
+    lastname = browser.find_element(by=By.ID, value='last-name')
+    postalcode = browser.find_element(by=By.ID, value='postal-code')
+    continue_button = browser.find_element(by=By.ID, value='continue')
+    firstname.send_keys("Naufal")
+    lastname.send_keys("Azhar")
+    postalcode.send_keys("123")
+    continue_button.click()
+
+    # Ensure the purchase is successful
+    assert "Checkout: Overview" in browser.page_source
+    assert "Payment Information" in browser.find_element(by=By.CLASS_NAME, value="summary_info_label").text
+
+    confirm_purchase_button = browser.find_element(by=By.ID, value='finish')
+    confirm_purchase_button.click()
+
+    # Complete Checkout
+    assert browser.find_element(by=By.XPATH, value="//div[@class='checkout_complete_container']").is_displayed()
+    assert 'Thank you for your order!' in browser.find_element(by=By.CSS_SELECTOR, value='h2').text
+    sleep(4)
+
+# ==================================================================================================
+
+def test_invalid_checkout(browser):
+    browser.get('https://www.saucedemo.com/')
+    assert 'Swag Labs' in browser.title
+
+    username = browser.find_element(by=By.ID, value='user-name')
+    password = browser.find_element(by=By.ID, value='password')
+    login_button = browser.find_element(by=By.ID, value='login-button')
+    username.send_keys("standard_user")
+    password.send_keys("secret_sauce")
+    login_button.click()
+    sleep(2)
+    assert "Products" in browser.page_source and "Swag Labs" in browser.page_source
+
+    item = browser.find_element(by=By.ID, value='add-to-cart-sauce-labs-bike-light')
+    item.click()
+    assert "Remove" in browser.find_element(by=By.XPATH, value="//button[@name='remove-sauce-labs-bike-light']").text
+    assert "1" in browser.find_element(by=By.CLASS_NAME, value="shopping_cart_badge").text
+    sleep(2)
+
+    cart_button = browser.find_element(by=By.CSS_SELECTOR, value='.shopping_cart_badge')
+    cart_button.click()
+    sleep(2)
+    assert "Your Cart" in browser.page_source
+    assert "$9.99" in browser.find_element(by=By.CLASS_NAME, value="inventory_item_price").text
+
+    checkout_button = browser.find_element(by=By.ID, value='checkout')
+    checkout_button.click()
+    sleep(2)
+
+    assert "Checkout: Your Information" in browser.page_source
+    assert browser.find_element(by=By.CLASS_NAME, value="checkout_info").is_displayed()
+    firstname = browser.find_element(by=By.ID, value='first-name')
+    lastname = browser.find_element(by=By.ID, value='last-name')
+    postalcode = browser.find_element(by=By.ID, value='postal-code')
+    continue_button = browser.find_element(by=By.ID, value='continue')
+    firstname.send_keys("Naufal")
+    lastname.send_keys("Azhar")
+    # postal code is left empty intentionally
+    postalcode.send_keys("")
+    continue_button.click()
+
+    # check if error message is displayed
+    assert browser.find_element(by=By.XPATH, value="//h3").is_displayed()
+    assert "Error: Postal Code is required" in browser.page_source
+    sleep(2)
 
 if __name__ == '__main__':
     pytest.main(['-v', '-s', 'test_script.py'])
